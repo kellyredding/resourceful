b3f31ecb11b0778f3fe0752839f5aafbff196b96
Kelly <kelly@kelredd.com>
2009-08-03 17:19:58 -0500

    reorg some tests in prep for mechanize tests

--- test/features/mechanize.feature
+++ test/features/mechanize.feature
@@ -1,4 +1,4 @@
-Feature: Get Resource
+Feature: Get Resource using mechanize agent
   In order to consume rest based resources
   As an object
   I want to get data from a web resource as a formatted object
@@ -12,50 +12,50 @@ Feature: Get Resource
     Then the format should be .xml
   
   Scenario: Host not configured
-    Given I have no RestClient host server configured
+    Given I have no host server configured
     Then resourceful should complain about a configuration error
 
   Scenario: Host configured
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     Then the host should be set
 
   Scenario: Resource logger
-    Given I have a configured RestClient resource host set to log
+    Given I have a configured resource host set to log
     Then verify the log settings
 
   Scenario: Resource logging
-    Given I have a configured RestClient resource host set to log
+    Given I have a configured resource host set to log
     When I get a json formatted resource
     Then the set log file should exist
 
 
 
   Scenario: Get JSON resource
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get a json formatted resource
     Then the result should be a hash object
     
   Scenario: Get XML resource
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get an xml formatted resource
     Then the result should be an xml object
     
   Scenario: Get an implicit format JSON resource
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get a json formatted implicitly resource
     Then the result should be a hash object
     
   Scenario: Get an implicit format XML resource
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get an xml formatted implicitly resource
     Then the result should be an xml object
     
   Scenario: Get resource with format not supported
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get an poop formatted resource
     Then resourceful should complain about a format error
     
   Scenario: Get non existent resource
-    Given I have a configured RestClient resource host
+    Given I have a configured resource host
     When I get a resource that does not exist
     Then resourceful should complain about the resource not being found
--- test/features/step_definitions/rest_client_steps.rb
+++ test/features/step_definitions/rest_client_steps.rb
@@ -1,16 +1,12 @@
-Given /^I want to use the (.+) resource format$/ do |format|
-  @format = Resourceful::Resource::Format.get(format)
-end
-
-Given /^I have no host server configured$/ do
+Given /^I have no RestClient host server configured$/ do
   @agent = Resourceful::Agent::RestClient.new
 end
 
-Given /^I have a configured resource host$/ do
+Given /^I have a configured RestClient resource host$/ do
   @agent = Resourceful::Agent::RestClient.new(:host => RESOURCE_CONFIG[:host])
 end
 
-Given /^I have a configured resource host set to log$/ do
+Given /^I have a configured RestClient resource host set to log$/ do
   log_file = File.expand_path(RESOURCE_CONFIG[:log])
   FileUtils.rm(log_file) if File.exists?(log_file)
   @agent = Resourceful::Agent::RestClient.new(:host => RESOURCE_CONFIG[:host]) {
@@ -18,67 +14,3 @@ Given /^I have a configured resource host set to log$/ do
   }
 end
 
-Then /^the format should be \.(.+)$/ do |format|
-  assert_equal @format.to_s, format
-end
-
-Then /^the host should be set$/ do
-  assert_equal @agent.host, RESOURCE_CONFIG[:host]
-end
-
-Then /^resourceful should complain about a configuration error$/ do
-  assert_raise Resourceful::Exceptions::ConfigurationError do
-    @agent.get RESOURCE_CONFIG[:resource], :format => 'json', :params => RESOURCE_CONFIG[:params]
-  end
-end
-
-Then /^verify the log settings$/ do
-  assert_equal @agent.logger.outputters.detect{|out| out.respond_to?('filename')}.filename, RESOURCE_CONFIG[:log]
-end
-
-Then /^the set log file should exist$/ do
-  log_file = File.expand_path(RESOURCE_CONFIG[:log])  
-  assert File.exists?(log_file)
-  FileUtils.rm(log_file) if File.exists?(log_file)
-end
-
-
-
-
-When /^I get a[n]* (.+) formatted resource$/ do |format|
-  @result = ResourcefulFeature::Helpers.safe_run_get do
-    @agent.get RESOURCE_CONFIG[:resource], :format => format, :params => RESOURCE_CONFIG[:params]
-  end
-end
-
-When /^I get a[n]* (.+) formatted implicitly resource$/ do |format|
-  @result = ResourcefulFeature::Helpers.safe_run_get do
-    @agent.get RESOURCE_CONFIG[:resource]+".#{format}", :params => RESOURCE_CONFIG[:params]
-  end
-end
-
-When /^I get a resource that does not exist$/ do
-  @result = ResourcefulFeature::Helpers.safe_run_get do
-    @agent.get '/unknown', :format => 'xml', :params => RESOURCE_CONFIG[:params]
-  end
-end
-
-Then /^the result should be a hash object$/ do
-  assert_kind_of Hash, @result.first
-end
-
-Then /^the result should be an xml object$/ do
-  assert_kind_of Nokogiri::XML::Document, @result
-end
-
-Then /^resourceful should complain about a format error$/ do
-  assert @result
-  assert_kind_of Resourceful::Exceptions::FormatError, @result
-  assert @result.message.length > 0
-end
-
-Then /^resourceful should complain about the resource not being found$/ do
-  assert @result
-  assert_kind_of RestClient::ResourceNotFound, @result
-  assert @result.message.length > 0
-end
