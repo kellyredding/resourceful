a24db99a65f250a268cf723dde2f4d4011f104bd
Kelly <kelly@kelredd.com>
2009-09-21 11:19:02 -0500

    missed the naming of the module

--- lib/resourceful/model/activerecord_associations.rb
+++ lib/resourceful/model/activerecord_associations.rb
@@ -1,6 +1,6 @@
 module Resourceful
   module Model
-    module ExternalAssociations
+    module ActiverecordAssociations
       
       module ClassMethods
 
--- lib/resourceful/version.rb
+++ lib/resourceful/version.rb
@@ -3,7 +3,7 @@ module Resourceful
     
     MAJOR = 0
     MINOR = 7
-    TINY  = 9
+    TINY  = 10
     
     def self.to_s # :nodoc:
       [MAJOR, MINOR, TINY].join('.')
