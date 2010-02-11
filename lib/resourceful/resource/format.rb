6279e4b513ed79efcf54e522200c595cb52267cd
Kelly <kelly@kelredd.com>
2010-02-11 13:36:07 -0600

    more complete fix for issue #1

--- lib/resourceful/resource/format.rb
+++ lib/resourceful/resource/format.rb
@@ -8,40 +8,60 @@ module Resourceful::Resource
     
     SUPPORTED = ['json', 'xml']
     
-    def self.get(format)
-      raise Resourceful::Exceptions::FormatError, "the format '#{format}' is not supported" unless SUPPORTED.include?(format.to_s)
-      self.send(format)
+    class << self
+      
+      def get(format)
+        raise Resourceful::Exceptions::FormatError, "the format '#{format}' is not supported" unless SUPPORTED.include?(format.to_s)
+        self.send(format)
+      end
+
+      def json
+        Resourceful::Resource::Json
+      end
+
+      def xml
+        Resourceful::Resource::Xml
+      end
+
+      def to_s
+        raise 'method not implemented by this format'
+      end
+
+      def build(str)
+        raise 'method not implemented by this format'
+      end
+      
+      protected
+      
+      def parsable?(data)
+        if !data.nil? && !data.to_s.strip.empty? && data.to_s.strip != 'null'
+          yield if block_given?
+        else
+          nil
+        end
+      end
+
     end
     
-    def self.json
-      Resourceful::Resource::Json
-    end
-      
-    def self.xml
-      Resourceful::Resource::Xml
-    end
-    
-    def self.to_s
-      raise 'method not implemented by this format'
-    end
-      
-    def self.build(str)
-      raise 'method not implemented by this format'
-    end
-      
   end
 end
 
 module Resourceful::Resource
   class Json < Resourceful::Resource::Format
     
-    def self.to_s
-      "json"
-    end
-      
-    def self.build(json_str)
-      !json_str.nil? && !json_str.to_s.empty? && (json_str.to_s != 'null') ? JSON.parse(json_str.to_s) : nil
-    end
+    class << self
+
+      def to_s
+        "json"
+      end
+
+      def build(json_str)
+        parsable?(json_str) do
+          JSON.parse(json_str.to_s.strip)
+        end
+      end
+
+    end    
       
   end
 end
@@ -49,13 +69,19 @@ end
 module Resourceful::Resource
   class Xml < Resourceful::Resource::Format
     
-    def self.to_s
-      "xml"
+    class << self
+    
+      def to_s
+        "xml"
+      end
+
+      def build(xml_str)
+        parsable?(xml_str) do
+          Nokogiri::XML(xml_str.to_s)
+        end
+      end
+
     end
-      
-    def self.build(xml_str)
-      Nokogiri::XML(xml_str.to_s)
-    end
-      
+    
   end
 end
